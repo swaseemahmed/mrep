434009b5bd15420ffdb8847e59ee12c1d3d54192
swaseemahmed <41055503+swaseemahmed@users.noreply.github.com>
2018-08-17 10:16:53 +0530

    Add files via upload

---
+++ archrekj/src/main/java/war/Application.java
@@ -0,0 +1,20 @@
+package war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import war.config.Mixer2EngineSingleton;
+import org.mixer2.Mixer2Engine;
+import java.util.concurrent.CompletableFuture;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.setProperty("com.sun.xml.internal.bind.v2.runtime.JAXBContextImpl.fastBoot","true");
+        CompletableFuture<Mixer2Engine> mixer2EngineCompletableFuture
+                = CompletableFuture.supplyAsync(() -> Mixer2EngineSingleton.getInstance());
+
+        SpringApplication.run(Application.class, args);
+    }
+}
